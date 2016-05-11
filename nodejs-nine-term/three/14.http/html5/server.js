3283c9558fbcc3db5c7d881fe9fb9ce903dc4481
吉会田 <jihuitian@souyidai.com>
2016-05-11 18:18:44 +0800

    http上传

--- nodejs-nine-term/three/14.http/html5/server.js
+++ nodejs-nine-term/three/14.http/html5/server.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var url = require("url");
 var formidable = require("formidable");
 var querystring = require("querystring");
+var path = require("path");
 
 var app = http.createServer(function(req,res){
     var urlObj = url.parse(req.url,true);
@@ -12,8 +13,33 @@ var app = http.createServer(function(req,res){
         fs.createReadStream("./index.html").pipe(res);
     }else if(pathname == "/post"){
         var parser = new formidable.IncomingForm();
-        parser.parse(req,function(err,fields,files){
+        parser.parse(req,function(err,fields,files){//fields->对象；files->文件
+            var filename = fields.name;
+            var total = fields.total;
+            var index = fields.index;
+            var size = fields.size;
+            var src = fs.createReadStream(files.data.path);
+            var target = fs.createWriteStream(filename+"."+index);
+            target.on("end",function(){
+                if(successCount == total - 1){
+                    var fd = fs.openSync(filename,"a");
+                    var files = fs.readdirSync(".");
+                    files.forEach(function(file){
+                        if(file.startsWith(filename+".")){
+                            var pos = (Number(path.extname(file).slice(1))-1)*size;
+                            var buff = fs.readdirSync(file);
+                            fs.writeSync(fd,buff,0,buff.length,pos);
+                            fs.unlinkSync(file);
 
+                        }
+                    });
+                    fs.closeSync(fd);
+                    successCount = 0;
+                }else{
+                    successCount++;
+                }
+            });
+            src.pipe(target);
         });
     }else{
         res.end("404");
